64ed07a1f4d3934ac7d4e762a3b0569ea2a373de
Katie Allen <kallen@spoonflower.com>
2018-06-01 12:28:49 -0500

    including most of the scaffolded stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :correspondences
+  resources :labels
+  resources :lists
+  resources :addresses
+  resources :contacts
+  resources :households
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
